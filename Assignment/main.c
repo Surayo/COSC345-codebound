bccf82a8cf16a335b0f444f934f968699739d65b
Marcus Anderson <andma305@student.otago.ac.nz>
2019-04-04 12:51:26 +1300

    Initial Commit

---
+++ Assignment/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment
+//
+//  Created by Marcus Anderson on 4/4/19.
+//  Copyright © 2019 CodeBound. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
