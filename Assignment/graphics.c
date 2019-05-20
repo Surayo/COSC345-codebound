2a9cc968d3e43da6e62c25008228ac1029742cbd
Josef Bode <josefbode@outlook.com>
2019-05-20 13:29:52 +1200

    changes to graphics.c window stays up in fullscreen until press escape key

--- Assignment/graphics.c
+++ Assignment/graphics.c
@@ -12,31 +12,72 @@
 #include <stdio.h>
 
 void create_window(){
-    const int WIDTH = 800, HEIGHT = 600;
-
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-
-    window = SDL_CreateWindow("Game Window",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              WIDTH,
-                              HEIGHT,
-                              SDL_WINDOW_OPENGL);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
-
-    SDL_Delay(5000);
-
-
+    const int WIDTH = 1000, HEIGHT = 800;
+    
+    //GameState = ganeState;
+    SDL_Window *window;                     //Declare a window
+    SDL_Renderer *renderer;                 //Declare a renderer
+    
+    SDL_Init(SDL_INIT_VIDEO);               //Initialise SDL2
+    
+    //create an application window with the following settings:
+    window = SDL_CreateWindow("Game Window",                //window title
+                              SDL_WINDOWPOS_UNDEFINED,      //initial x pos
+                              SDL_WINDOWPOS_UNDEFINED,      //initial y pos
+                              WIDTH,                        //width, in pixels
+                              HEIGHT,                       //height, in pixels
+                              SDL_WINDOW_FULLSCREEN);       //flags
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    
+    //the window is open: enter program loop (SDL_PollEvent)
+    int done = 0;
+    SDL_Event event;
+    
+    //Event loop:
+    while(!done) {
+        //check for events:
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_WINDOWEVENT_CLOSE: {
+                    if(window) {
+                        SDL_DestroyWindow(window);
+                        window = NULL;
+                    }
+                }
+                break;
+                case SDL_KEYDOWN: {
+                    switch(event.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                            done = 1;
+                        break;
+                    }
+                }
+                break;
+                case SDL_QUIT:
+                    //quit out of the game
+                    done = 1;
+                break;
+            }
+        }
+        
+        //render display settings:
+        //set the drawing colour to gray
+        SDL_SetRenderDrawColor(renderer, 60, 60, 60, 255);
+        
+        //clear the screen to gray
+        SDL_RenderClear(renderer);
+        
+        //present what we have drawn
+        SDL_RenderPresent(renderer);
+        
+        //slight delay (don't burn out CPU)
+        SDL_Delay(100);
+    }
+    
+    //close and destroy the window
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
 
+    //clean up
     SDL_Quit();
 }
