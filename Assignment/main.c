6c5cdb3236abfc3306a5f62f12f5975d356e614d
Marcus Anderson <andma305@student.otago.ac.nz>
2019-05-27 16:14:32 +1200

    I missed the option

--- Assignment/main.c
+++ Assignment/main.c
@@ -21,59 +21,64 @@ int main(int argc, const char * argv[]) {
     char *text = NULL;
     char *choice = NULL, *next_file = NULL;
     FILE *fptr;
-    int choice_num, check;
+    int choice_num, check, type;
     
-    cwd = emalloc(150 * sizeof(char));
-    //file_location = emalloc(50 * sizeof(char));
-    
-    cwd = current_directory(cwd);
-    strcpy(file_location, "/Devolution/C0.txt");
-    //fptr is a pointer to the story text
-    while (true){
-        fptr = openfile(cwd, file_location);
-        if (fptr == NULL){
-            break;
-        }
-        
-        text = setFile(fptr);
-
-        store_brackets(text);
-        text = getCleanText();
-        printf("%s\n", text);
-        
-        choice = getChoiceText(1);
-        printf("%s\n", choice);
-        choice = getChoiceText(2);
-        printf("%s\n", choice);
-        choice = getChoiceText(3);
-        printf("%s\n", choice);
-        
-        printf("Please enter a value between 1 and 3:\n");
-        check = scanf("%d", &choice_num);
-        if (check != 1){
-            printf("scanf returned code %d\n", check);
-            return EXIT_FAILURE;
-        }
-        
-        next_file = getNextFile(choice_num);
-        //printf("%s\n", next_file);
-    
-        //file_location = NULL;
-        strcpy(file_location, "/Devolution/");
-        strcat(file_location, next_file);
-        strcat(file_location, ".txt");
-        //choice_num = 0;
-    
-        freeChoices();
-        //createWindow(1);
-        fclose(fptr);
+    printf("Please enter 1 for gui and 2 for text based game:\n");
+    check = scanf("%d", &type);
+    if (check != 1){
+        printf("scanf returned code %d\n", check);
+        return EXIT_FAILURE;
     }
-    
-    free(cwd);
+    if (type == 1){
+        createWindow(1);
+    } else {
+        cwd = emalloc(150 * sizeof(char));
+        //file_location = emalloc(50 * sizeof(char));
+        
+        cwd = current_directory(cwd);
+        strcpy(file_location, "/Devolution/C0.txt");
+        //fptr is a pointer to the story text
+        while (true){
+            fptr = openfile(cwd, file_location);
+            if (fptr == NULL){
+                break;
+            }
+            
+            text = setFile(fptr);
+
+            store_brackets(text);
+            text = getCleanText();
+            printf("%s\n", text);
+            
+            choice = getChoiceText(1);
+            printf("%s\n", choice);
+            choice = getChoiceText(2);
+            printf("%s\n", choice);
+            choice = getChoiceText(3);
+            printf("%s\n", choice);
+            
+            printf("Please enter a value between 1 and 3:\n");
+            check = scanf("%d", &choice_num);
+            if (check != 1){
+                printf("scanf returned code %d\n", check);
+                return EXIT_FAILURE;
+            }
+            
+            next_file = getNextFile(choice_num);
+            //printf("%s\n", next_file);
+        
+            //file_location = NULL;
+            strcpy(file_location, "/Devolution/");
+            strcat(file_location, next_file);
+            strcat(file_location, ".txt");
+            //choice_num = 0;
+        
+            freeChoices();
+            fclose(fptr);
+        }
+        
+        free(cwd);
+    }
     
     return EXIT_SUCCESS;
 }
-
-/*
-    YOU ARE BREAKING UP THE TEXT FILES LOOK AT ME!!!!!!!!!! REMEMBER
- */
