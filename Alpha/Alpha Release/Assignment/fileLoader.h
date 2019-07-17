95eff95a8a997197a706add0968964e345007aed
Marcus Anderson <andma305@student.otago.ac.nz>
2019-07-17 14:39:10 +1200

    Cleaning big Commit

---
+++ Alpha/Alpha Release/Assignment/fileLoader.h
@@ -0,0 +1,17 @@
+//
+//  fileLoader.h
+//  Assignment
+//
+//  Created by Marcus Anderson on 5/2/19.
+//  Copyright © 2019 CodeBound. All rights reserved.
+//
+
+#ifndef fileLoader_h
+#define fileLoader_h
+
+extern char* concat(const char *s1, const char *s2);
+extern char* current_directory(char *cwd);
+extern FILE* openfile(char *cwd, char *filename);
+extern void closefile(FILE *fptr);
+
+#endif /* fileLoader_h */
